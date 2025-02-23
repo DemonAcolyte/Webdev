42ed604c4521183635301ecb308abcccb1afbf1e
MIGGY CARUMBA <miggycarumba912@gmail.com>
2025-02-23 10:58:09 +0800

    fixed login

--- login.php
+++ login.php
@@ -1,22 +1,47 @@
 
 <?php
-
+    session_start();
     include("database.php");
 
 
-    if ()
+    if ($conn->connect_error){
+        die("Connection Failed: ". $conn->connect_error);
+    }
+    if ($_SERVER['REQUEST METHOD'] == 'POST'){
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
 
+
+        if (empty($username) || empty($password)){
+            die("Username and Password is required");
+        }
+    
+
+
+    $sql = "SELECT username, password FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+   
+    if ($stmt->num_rows > 0){
+        $stmt->bind_result($db_username, $db_password);
+        $stmt->fetch();
+    
+
+        if (password_verify($password, $db_password)){
+        $_SESSION['username'] = $db_username;
+        echo "Login Successful! Welcome $db_username!";
+        } else{
+        echo "Invalid Password";
+        }
+
+    } else{
+    echo"User not found";
+    }
+    $stmt->close();
+    }
+    $conn->close();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
